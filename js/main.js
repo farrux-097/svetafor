e9c0119741b937cccd4ee9d434ec94bb8fefd7da
farrux-097 <olimovfarrux907@gmail.com>
2024-06-28 01:59:18 +0500

    Finish svetafor

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let elCard1 = document.querySelector(".card1")
+let elCard2 = document.querySelector(".card2")
+let elCard3 = document.querySelector(".card3")
+
+
+
+function ChangeColor (){
+    setTimeout(() =>{
+        elCard1.classList.add("red")
+        elCard2.classList.remove("yellow")
+        elCard3.classList.remove("green")
+    },0)
+    setTimeout(() =>{
+        elCard2.classList.add("yellow")
+        elCard1.classList.remove("red")
+        elCard3.classList.remove("green")
+    },2000)
+
+    setTimeout(() =>{
+        elCard1.classList.remove("red")
+        elCard2.classList.remove("yellow")
+        elCard3.classList.add("green")
+    },4000)
+    
+}
+
+ChangeColor()
+
+setInterval(() => {
+    console.log("interval start");
+    ChangeColor()
+},6000)
